9553a605616dfc8b943fd434dba196e9045d17e0
Winderson Santos <winderson.wjbs@gmail.com>
2024-07-15 23:47:26 -0300

    fix: remove classes dto e schema

--- src/core/application/services/productService.ts
+++ src/core/application/services/productService.ts
@@ -1,20 +1,19 @@
 import { ProductRepository } from '@ports/productRepository';
-import { productCategorySchema, ProductCategoryDto } from '@driver/schemas/productCategorySchema';
 import { Product, ProductCategory } from '@prisma/client';
 
 export class ProductService {
 	constructor(private readonly productRepository: ProductRepository) { }
 
 	async getProducts(): Promise<Product[]> {
-		return [];
+		return this.productRepository.getProducts();
 	}
 
 	async getProductCategories(): Promise<ProductCategory[]> {
 		return this.productRepository.getProductCategories();
 	}
 
-	async createProductCategory(productCategoryDto: ProductCategoryDto): Promise<ProductCategory> {
-		productCategoryDto = productCategorySchema.parse(productCategoryDto);
+	async createProductCategory(productCategoryDto: ProductCategory): Promise<ProductCategory> {
+		// productCategoryDto = productCategorySchema.parse(productCategoryDto);
 		return this.productRepository.createProductCategory({ ...productCategoryDto });
 	}
 }
