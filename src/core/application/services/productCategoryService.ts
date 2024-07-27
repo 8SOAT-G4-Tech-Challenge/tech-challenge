aa84f03aff481cd47e81dc7f6336ab676b788e37
Winderson Santos <winderson.wjbs@gmail.com>
2024-07-27 13:19:38 -0300

    fix: ajusta Eslint e erros reportados

--- src/core/application/services/productCategoryService.ts
+++ src/core/application/services/productCategoryService.ts
@@ -1,16 +1,15 @@
-
+import { productCategoryCreateSchema } from '@driver/schemas/productCategorySchema';
 import { ProductCategoryRepository } from '@ports/repository/productCategoryRepository';
 import { ProductCategory } from '@prisma/client';
-import { productCategoryCreateSchema } from '@driver/schemas/productCategorySchema';
 
 export class ProductCategoryService {
 	constructor(private readonly productCategoryRepository: ProductCategoryRepository) {}
 
-    async getProductCategories(): Promise<ProductCategory[]> {
+	async getProductCategories(): Promise<ProductCategory[]> {
 		return this.productCategoryRepository.getProductCategories();
 	}
 
-    async getProductCategoryByName(category: string): Promise<ProductCategory | null> {
+	async getProductCategoryByName(category: string): Promise<ProductCategory | null> {
 		return this.productCategoryRepository.getProductCategoryByName(category);
 	}
 
