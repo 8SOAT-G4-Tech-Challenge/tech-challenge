67fa4ce6c2bf12b81bc9b0cbe8652c045b28df38
Winderson Santos <winderson.wjbs@gmail.com>
2024-07-27 13:07:17 -0300

    fix: remove product do prisma

--- src/core/application/services/productService.ts
+++ src/core/application/services/productService.ts
@@ -1,26 +1,25 @@
-import { ProductRepository } from '@ports/repository/productRepository';
-import { Product } from '@prisma/client';
-import { productFilterSchema } from '@driver/schemas/productSchema';
-import { ProductCategoryService } from '@services/productCategoryService';
 import logger from '@common/logger';
+import { productFilterSchema } from '@driver/schemas/productSchema';
+import { Product } from '@models/product';
+import { ProductRepository } from '@ports/repository/productRepository';
+import { ProductCategoryService } from '@services/productCategoryService';
 
 export class ProductService {
-	constructor(private readonly productCategoryService: ProductCategoryService, 
+	constructor(private readonly productCategoryService: ProductCategoryService,
 		private readonly productRepository: ProductRepository) {}
 
 	async getProducts(filters: any): Promise<Product[]> {
 		productFilterSchema.parse(filters);
 		if (filters.category) {
 			logger.info(`Searching category by name: ${filters.category}`);
-			const productCategory = await this.productCategoryService.getProductCategoryByName(filters.category);
+			const productCategory =
+				await this.productCategoryService.getProductCategoryByName(filters.category);
 			if (productCategory) {
-				logger.info(`Success search product category ${JSON.stringify(productCategory)}`)
+				logger.info(`Success search product category ${JSON.stringify(productCategory)}`);
 				return this.productRepository.getProductsByCategory(productCategory.id);
-			} else {
-				return [];
 			}
-		} else {
-			return this.productRepository.getProducts();
+			return [];
 		}
+		return this.productRepository.getProducts();
 	}
 }
