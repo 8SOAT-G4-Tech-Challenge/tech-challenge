54fe1f2a43983e5b7cf25f0ac199a71d8b5a3fa6
Jackson Batista <jackson.batista@mblabs.com.br>
2025-02-07 11:30:49 -0300

    feat: remove all unused assets

---
+++ src/adapter/driver/routes/index.ts
@@ -0,0 +1,121 @@
+import { FastifyInstance } from 'fastify';
+
+import {
+	CartService,
+	OrderService,
+	ProductCategoryService,
+	ProductService,
+} from '@application/services';
+import {
+	CartRepositoryImpl,
+	FileSystemStorageImpl,
+	OrderRepositoryImpl,
+	ProductCategoryRepositoryImpl,
+	ProductImageRepositoryImpl,
+	ProductRepositoryImpl,
+} from '@driven/infra';
+import {
+	CartController,
+	OrderController,
+	ProductCategoryController,
+	ProductController,
+} from '@driver/controllers';
+
+const productRepository = new ProductRepositoryImpl();
+const orderRepository = new OrderRepositoryImpl();
+const productCategoryRepository = new ProductCategoryRepositoryImpl();
+const productImageRepository = new ProductImageRepositoryImpl();
+const fileSystemStorage = new FileSystemStorageImpl();
+const cartRepository = new CartRepositoryImpl();
+
+const productCategoryService = new ProductCategoryService(
+	productCategoryRepository
+);
+const productService = new ProductService(
+	productCategoryService,
+	productRepository,
+	productImageRepository,
+	fileSystemStorage
+);
+
+const orderService = new OrderService(orderRepository, cartRepository);
+const cartService = new CartService(
+	cartRepository,
+	orderRepository,
+	productRepository
+);
+
+const productCategoryController = new ProductCategoryController(
+	productCategoryService
+);
+const productController = new ProductController(productService);
+const orderController = new OrderController(orderService);
+const cartController = new CartController(cartService);
+
+export const routes = async (fastify: FastifyInstance) => {
+	fastify.get(
+		'/totem/products',
+		productController.getProducts.bind(productController)
+	);
+	fastify.post(
+		'/admin/products',
+		productController.createProducts.bind(productController)
+	);
+	fastify.put(
+		'/admin/products/:id',
+		productController.updateProducts.bind(productController)
+	);
+	fastify.delete(
+		'/admin/products/:id',
+		productController.deleteProducts.bind(productController)
+	);
+	fastify.post(
+		'/admin/product-categories',
+		productCategoryController.createProductCategory.bind(
+			productCategoryController
+		)
+	);
+	fastify.get(
+		'/totem/product-categories',
+		productCategoryController.getProductCategories.bind(
+			productCategoryController
+		)
+	);
+	fastify.put(
+		'/admin/product-categories/:id',
+		productCategoryController.updateProductCategories.bind(
+			productCategoryController
+		)
+	);
+	fastify.delete(
+		'/admin/product-categories/:id',
+		productCategoryController.deleteProductCategories.bind(
+			productCategoryController
+		)
+	);
+	fastify.get('/admin/orders', orderController.getOrders.bind(orderController));
+	fastify.get(
+		'/totem/orders/:id',
+		orderController.getOrderById.bind(orderController)
+	);
+	fastify.post(
+		'/totem/orders',
+		orderController.createOrder.bind(orderController)
+	);
+	fastify.put(
+		'/totem/orders/:id',
+		orderController.updateOrder.bind(orderController)
+	);
+	fastify.post(
+		'/totem/order-items/:orderId',
+		cartController.addItemToCart.bind(cartController)
+	);
+	fastify.put(
+		'/totem/order-items/:id',
+		cartController.updateCartItem.bind(cartController)
+	);
+	fastify.delete(
+		'/totem/order-items/:id',
+		cartController.deleteCartItem.bind(cartController)
+	);
+};
